0e97836c9e6a9c4d64f2e092ef3e08e8badcfa1f
virtualynx <virtualynx@gmail.com>
2025-10-25 20:29:49 +0700

    add seeder for: - spm_level - survey question TK - survey question KB - survey question SPS

---
+++ database/seeders/SpmLevelSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SpmLevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'spm_level_id' => 'SPM_TK',
+                'name' => 'Persyaratan SPM TK',
+                'created_at' => now(),
+                'created_by' => 'system',
+                'is_disabled' => false,
+            ],
+            [
+                'spm_level_id' => 'SPM_KB',
+                'name' => 'Persyaratan SPM KB',
+                'created_at' => now(),
+                'created_by' => 'system',
+                'is_disabled' => false,
+            ],
+            [
+                'spm_level_id' => 'SPM_SPS',
+                'name' => 'Persyaratan SPM SPS',
+                'created_at' => now(),
+                'created_by' => 'system',
+                'is_disabled' => false,
+            ],
+        ];
+        
+        DB::table('spm_level')->insert($data);
+    }
+}
