cb52d49322d4996d8137cddba01792f21319fc00
virtualynx <virtualynx@gmail.com>
2025-10-26 10:44:32 +0700

    add columns on table school: - spm_level_id - student_count - rombel_a_count - rombel_b_count

---
+++ database/migrations/2025_10_26_000003_mod_school_add_rombel_fields.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('school', function (Blueprint $table) {
+            $table->string('spm_level_id')->after('NPSN');
+
+            $table->integer('student_count')->nullable();
+            $table->integer('rombel_a_count')->nullable();
+            $table->integer('rombel_b_count')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
